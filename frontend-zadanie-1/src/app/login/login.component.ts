3cf50ffada296f6e4a7012619a0f96b93b0d8312
rafal.nowocinski <vito24222@gmail.com>
2025-10-30 14:48:56 +0100

    Updated LoginComponent

--- frontend-zadanie-1/src/app/login/login.component.ts
+++ frontend-zadanie-1/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,9 +11,30 @@ import { FormsModule } from '@angular/forms';
 export class LoginComponent {
   email = '';
   password = '';
+  errorMessage = '';
+  isLoading = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   onLogin() {
-    // TODO: Implement
-    console.log({ email: this.email, password: this.password });
+    this.errorMessage = '';
+    this.isLoading = true;
+
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        this.authService.saveTokens(response.accessToken, response.refreshToken);
+        this.isLoading = false;
+        // TODO: Navigate to main page
+        console.log('Login successful', response);
+      },
+      error: (error) => {
+        this.isLoading = false;
+        this.errorMessage = 'Invalid email or password';
+        console.error('Login failed', error);
+      }
+    });
   }
 }
