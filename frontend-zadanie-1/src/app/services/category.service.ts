f3360a80b5a45b90aa5b1fc88c08153ec39978e8
rafal.nowocinski <vito24222@gmail.com>
2025-10-31 06:03:07 +0100

    Create category.service.ts

---
+++ frontend-zadanie-1/src/app/services/category.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Category {
+  id: string;
+  name: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  private readonly apiUrl = 'http://localhost:5088/api/categories';
+
+  constructor(private readonly http: HttpClient) {}
+
+  getCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(this.apiUrl);
+  }
+}
