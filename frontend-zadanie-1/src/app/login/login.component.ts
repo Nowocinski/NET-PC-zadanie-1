e75fe5e6b8216795b0976424776659459514c127
rafal.nowocinski <vito24222@gmail.com>
2025-11-01 10:16:42 +0100

    Signals

--- frontend-zadanie-1/src/app/login/login.component.ts
+++ frontend-zadanie-1/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
@@ -11,8 +11,8 @@ import { AuthService } from '../services/auth.service';
 export class LoginComponent {
   email = '';
   password = '';
-  errorMessage = '';
-  isLoading = false;
+  errorMessage = signal('');
+  isLoading = signal(false);
 
   constructor(
     private authService: AuthService,
@@ -20,18 +20,18 @@ export class LoginComponent {
   ) {}
 
   onLogin() {
-    this.errorMessage = '';
-    this.isLoading = true;
+    this.errorMessage.set('');
+    this.isLoading.set(true);
 
     this.authService.login(this.email, this.password).subscribe({
       next: (response) => {
         this.authService.saveTokens(response.accessToken, response.refreshToken);
-        this.isLoading = false;
+        this.isLoading.set(false);
         this.router.navigate(['/']);
       },
       error: (error) => {
-        this.isLoading = false;
-        this.errorMessage = 'Invalid email or password';
+        this.isLoading.set(false);
+        this.errorMessage.set('Invalid email or password');
         console.error('Login failed', error);
       }
     });
